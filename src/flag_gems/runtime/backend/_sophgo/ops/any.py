[fix] avoid DMA stride overflow in dim_compress for sophgo backend

src/flag_gems/runtime/backend/_sophgo/ops/any.py
@@ -7,8 +7,10 @@ import triton.language as tl
 
 from flag_gems import runtime
 from flag_gems.runtime import torch_device_fn
-from flag_gems.utils import dim_compress, libentry
+from flag_gems.utils import libentry
 from flag_gems.utils import triton_lang_extension as tle
+
+from ..utils.shape_utils import dim_compress
 
 # torch.any: Tests if any elements in input evaluate to True. If the dtype of input
 #            is not BOOL, then test if any elements in input evaluate to non-zero value